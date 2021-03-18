314692af0bae49cabccedd259e6f6936d08495dc
MJTSgamer <78482881+MJTSgamer@users.noreply.github.com>
2021-03-18 09:43:23 +0100

    Add files via upload

---
+++ portfolio/assets/js/app.js
@@ -0,0 +1,106 @@
+﻿'use strict';
+
+let header = $(`
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+<a class="navbar-brand" href="index.html"> ＜／𝙼𝚊𝚝𝚝 𝚝𝚎𝚛 𝚂𝚝𝚎𝚎𝚐𝚎＞ </a>
+<div class="hamburger_wrapper navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> 
+
+  <div id="js-hamburger" class="hamburger">
+    <span class="first"></span>
+    <span class="second"></span>
+    <span class="third"></span>
+  </div>
+
+</div>
+
+<div class="collapse navbar-collapse " id="navbarSupportedContent">
+  <ul class="navbar-nav ml-auto" id = "navbar-content">
+   <li class="nav-item"><a class="nav-link" href="index.html"><b>Home();</b></a></li>
+ <!--  <li class="nav-item"><a class="nav-link" href="experience.html"><b>Experience();</b></a></li> -->
+   <li class="nav-item"><a class="nav-link" href="projects.html"><b>Projecten();</b></a></li>
+ <!--  <li class="nav-item"><a class="nav-link" href="education.html"><b>Education();</b></a></li> -->
+   <li class="nav-item"><a class="nav-link" href="ownprojects.html"><b>Eigen Projecten();</b></a></li>
+  </ul>
+</div>
+</nav>`);
+
+let footer = $(`
+<footer class="footer">
+  <div class="container p-4">
+    <div class="row">
+      <div class="col-lg-20 col-md-12 mb-4 mb-md-0 justify-content-center">
+        <p class="tag" >"Develop a passion for learning. If you do, you will never cease to grow."<br><br><br>
+        <p class="tag">deze website is bij lange na nog niet af, bijvoorbeeld deze tekst vol met bugs :( maar dat komt we goed.<br>
+        </p>  
+      </div>
+        <div class="col-lg-20 col-md-12 mb-4 mb-md-0">
+      </div>
+    </div>
+  </div>
+</footer>
+`);
+
+
+
+// Window Loads
+$(function () {
+    let bodyElement = $(`body`);
+    bodyElement.prepend(header);
+    bodyElement.append(footer);
+  
+//toggler hamburger functions
+    const menuBtn = document.querySelector('.navbar-toggler');
+    let menuOpen = false;
+    menuBtn.addEventListener('click', () => {
+      if(!menuOpen){
+        menuBtn.classList.add('open')
+        menuOpen = true;
+      }
+      else{
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+      }
+  });
+
+});
+
+// function for toggling hamburger is-active class
+$(function(){
+  
+  $("#js-hamburger").on("click", function(){
+    $(this).toggleClass('is-active');
+  });
+
+});
+
+// Navbar current page highlight
+
+let loader = document.querySelector('.loader-container');
+
+window.addEventListener("load", vanish);
+function vanish() {
+    loader.classList.add("disappear")
+}
+$(function(){
+  $('a.nav-link').each(function() {
+    if ($(this).prop('href') == window.location.href) {
+      $(this).addClass('current-link');
+    }
+  });
+});
+
+//function to remove underline on hover
+
+$(document).ready(function(){
+
+  $("a.nav-link").hover(
+       function () {
+         $(this).removeClass("current-link");
+       },
+       function () {
+        if ($(this).prop('href') == window.location.href) {
+          $(this).addClass('current-link');
+        }
+      }
+  );
+});
---
+++ portfolio/assets/js/dominoproject.js
@@ -0,0 +1,124 @@
+//  Work experience cards
+
+
+const experiencecards = document.querySelector(".experience-cards");
+const exp = [
+  {
+    title: "Domino-Day",
+        cardImage: "assets/images/projecten-page/dominobaan.jpg",
+    time: " ",
+    desp: "<li><b>Doel</b></br> Het doel is het klaar hebben van een deelconstructie voor je team als we straks weer naar school mogen.</li> <li><b>Opdracht gever</b></br> Wij hebben van meneer Nijkamp de opdracht gekregen om technische constructies voor domino banen te maken. Wat wij moesten doen was een constructie maken die de beweging van een rij domino stenen doorgeeft aan een andere rij dominostenen, met daartussen ‘obstakels’ of iets anders zoals een treintje.</li>",
+  },
+  {
+    title: "Leuk weetje",
+      cardImage: "assets/images/projecten-page/gravity.jpg",
+    time: " ",
+      desp: "<li>Is een jaarlijks evenement  rond een wereldrecordpoging voor het grootste </br>aantal vallende dominostenen opgezet door 1 groep mensen.</br> Het vond van 1998 tot en met 2009 jaarlijks plaats in november,</br> in 2020 keert het programma na elf jaar terug. De organisatie en productie</br> is in handen van Endemol. Het huidige record staat op 4.491.863</br> dominostenen. Het werd gevestigd op 13 november 2009 onder </br>leiding van Robin Paul Weijers.",
+    },
+];
+
+const showCards2 = () => {
+  let output = "";
+  exp.forEach(
+    ({ title, cardImage, time, desp }) =>
+      (output += `        
+      <ul>
+      <li class="card card1">
+        <img src="${cardImage}" class="featured-image"/>
+        <article class="card-body">
+          <header>
+            <div class="title">
+              <h3>${title}</h3>
+            </div>
+            <p class="meta">
+              <span class="author">${time}</span>
+            </p>
+            <ol>
+              ${desp}
+            </ol>
+          </header>
+        </article>
+      </li>
+    </ul>
+      `)
+  );
+  experiencecards.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards2);
+
+
+// Volunteership Cards
+
+const volunteership = document.querySelector(".volunteership");
+const volunteershipcards = [
+  {
+    title: "Matt ter Steege",
+    cardImage: "assets/images/projecten-page/dp_male.jpg",
+    description:
+      "Ik heb geholpen met het bedenken van de constructie",
+  },
+  {
+    title: "Nynke Boeijinga",
+      cardImage: "assets/images/projecten-page/dp_female.jpg",
+    description:
+      "Heeft een groot deel van de constructie bedacht en heeft heel veel dingen uitgetekend",
+  },
+  {
+    title: "Wanika Kroon",
+      cardImage: "assets/images/projecten-page/dp_female.jpg",
+    description:
+      "Heeft niet veel gedaan anders dan filmpjes van katten bekeken",
+  },
+  {
+    title: "Guus den Hartog",
+      cardImage: "assets/images/projecten-page/dp_male.jpg",
+    description:
+      "is naar een andere school gegaan dus heeft ook niet veel gedaan",
+  },
+];
+
+const showCards = () => {
+  let output = "";
+  volunteershipcards.forEach(
+    ({ title, cardImage, description }) =>
+      (output += `        
+      <div class="card volunteerCard" style="height: 600px;width:400px">
+      
+      <img src="${cardImage}" height="300" width="65" class="card-img" style="border-radius:10px">
+      <div class="content">
+          <h2 class="volunteerTitle">${title}</h2><br>
+          <p class="copy">${description}</p></div>
+      
+      </div>
+      `)
+  );
+  volunteership.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards);
+
+const showCards3 = () => {
+  let output = "";
+  mentor.forEach(
+    ({ title, image, time, desp}) =>
+      (output += `        
+      <div class="column mentorshipCard"> 
+      <div class="card card2 mentorshipCardCover">
+        <img src="${image}" class="card-img-top" alt="..."  width="64" height="300">
+        <div class="information">
+        <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <p class=""sub-title">${time}</p>
+        </div>
+        <div class="more-information">
+        <ul class="list-group list-group-flush p-0 right-aligned">
+          <div class="list-group-item card2 disclaimer">${desp}</div>
+        </ul>
+        </div>
+        </div>
+      </div>
+      </div>
+      `)
+  );
+  mentorshipcards.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards3);
